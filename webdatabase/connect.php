c26dae653f465348739bdc1dd5718cbc05839fd1
passaworn suwannamanee <79976116+ipleumi@users.noreply.github.com>
2024-03-27 13:30:16 +0700

    Add files via upload

---
+++ webdatabase/connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost"; /* Host name */
+$user = "root"; /* User */
+$password = "12345678"; /* Password */
+$dbname = "test"; /* Database name */
+
+$con = mysqli_connect($host, $user, $password,$dbname);
+// Check connection
+// if (!$con) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
+
+
+?>
+
